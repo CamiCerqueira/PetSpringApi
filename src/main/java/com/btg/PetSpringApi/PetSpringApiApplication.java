273ed4d7fb753a3f5b73c204cfc2cc6db25f1dc7
CamilaCerqueira <camilaroberia@gmail.com>
2023-09-30 19:14:55 -0300

    Initial commit

---
+++ src/main/java/com/btg/PetSpringApi/PetSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.btg.PetSpringApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btg/PetSpringApi/PetSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btg.PetSpringApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
