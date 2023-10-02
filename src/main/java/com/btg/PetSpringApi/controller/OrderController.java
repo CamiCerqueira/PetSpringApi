655c0a51a1e451bbc4ef11e3aa1b4af1165b35e5
Felicianne <felicianne_costa@hotmail.com>
2023-10-02 10:11:01 -0300

    ordercontroller

--- src/main/java/com/btg/PetSpringApi/controller/OrderController.java
+++ src/main/java/com/btg/PetSpringApi/controller/OrderController.java
@@ -1,4 +1,34 @@
 package com.btg.PetSpringApi.controller;
 
+import com.btg.PetSpringApi.controller.dto.OrderRequest;
+import com.btg.PetSpringApi.controller.dto.OrderResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<OrderResponse> saveOrder(@RequestBody OrderRequest orderRequest) {
+        OrderResponse orderReponse = orderService.saveOrder(orderRequest);
+        return ResponseEntity.created(URI.create("/order/" + orderReponse.getId())).body(orderReponse);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getOrder((
+       @RequestParam(name = "userId", required = false) Integer userId,
+       @RequestParam(name = "productId", required = false) Integer productId
+    ){
+        return ResponseEntity.ok(orderService.getAllOrders(userId, productId));
+    }
+
 }
