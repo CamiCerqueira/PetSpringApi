0b2e96c858cf43c592ced36258731b4ca36beccf
camila <camilaroberia@gmail.com>
2023-10-02 14:00:46 -0300

    Update Controller

---
+++ src/main/java/com/btg/PetSpringApi/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.btg.PetSpringApi.controller;
+
+import com.btg.PetSpringApi.controller.dto.ProductRequest;
+import com.btg.PetSpringApi.controller.dto.ProductResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProduct(
+            @RequestParam(name = "typeProduct", required = false) Integer typeProduct
+    ){
+        return ResponseEntity.ok(productService.getAllProduct(typeProduct));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> saveProduct(@RequestBody ProductRequest productRequest){
+        ProductResponse productResponse =  productService.saveProduct (productRequest);
+
+        return ResponseEntity.created(URI.create("/product/"+productResponse.getId())).body(productResponse);
+    }
+}
--- src/main/java/com/btg/PetSpringApi/controller/UserEmployeeController.java
+++ src/main/java/com/btg/PetSpringApi/controller/UserEmployeeController.java
@@ -1,5 +1,68 @@
 package com.btg.PetSpringApi.controller;
 
+import com.btg.PetSpringApi.controller.dto.UserEmployeeRequest;
+import com.btg.PetSpringApi.controller.dto.UserEmployeeResponse;
+import com.btg.PetSpringApi.model.UserEmployee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping ("/user")
 public class UserEmployeeController {
+    @Autowired
+    UserEmployeeService userEmployeeService;
+    @RequestMapping
+    public ResponseEntity <Page<UserEmployeeResponse>> getUsers(
+            @RequestParam(value = "page",
+                    required = false,
+                    defaultValue = "0"
+            ) int page,
+            @RequestParam(
+                    value = "size",
+                    required = false,
+                    defaultValue = "10"
+            ) int size,
+            @RequestParam(
+                    value = "direction",
+                    required = false,
+                    defaultValue = "ASC"
+            ) String direction)
+    {return ResponseEntity.ok(userEmployeeService.GetUserEmployeeService;
+            UserEmployeeResponse user =  userEmployeeService.saveUserEmployeeService (userDTO);
+            return ResponseEntity.created(URI.create("/user/"+user.getId())).body(user);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<UserEmployeeResponse> getUser(@PathVariable Integer id){
+            return ResponseEntity.ok(userEmployeeService.GetUserEmployeeService.ByResponse(id));
+        }
+
+        @GetMapping("/email/{email}")
+        public ResponseEntity<UserEmployeeResponse> getUserByEmail(@PathVariable String email){
+            return  ResponseEntity.ok(userEmployeeService.GetUserByEmail(email));
+        }
+
+        @GetMapping("/name/{name}")
+        public ResponseEntity<List<UserEmployeeResponse>> getAllUserByName(@PathVariable String name, @PathVariable Integer id){
+            return ResponseEntity.ok(userEmployeeService.getAllByName (name));
+        }
+
+        @DeleteMapping("/{id}")
+        public void deleteUser(@PathVariable Integer id){
+            userEmployeeService.deleteUser(id);
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<UserEmployeeResponse> updateUser(
+                @PathVariable Integer id,
+                @RequestBody UserEmployeeRequest userRequest
+    ){
+            return  ResponseEntity.ok(userEmployeeService.updateUser(id, userRequest));
 
 }
